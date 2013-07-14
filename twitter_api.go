d3efc17e97b9a9ed9a4982e395cbec32038cc028
Rozhnov Alexandr <nox73@ya.ru>
2013-07-14 18:03:01 +0400

    Send recived tweet to channel.

--- twitter_api.go
+++ twitter_api.go
@@ -3,14 +3,9 @@ package twitter_api
 import (
   "github.com/NOX73/go-oauth"
   "net/http"
-  "fmt"
   "bufio"
+  "bytes"
 )
-
-type Tweet struct {
-  id int
-  text string
-}
 
 type Credentials struct {
   oauth_consumer_key string
@@ -19,7 +14,53 @@ type Credentials struct {
   oauth_token_secret string
 }
 
-func TwitterStream (ch chan Tweet, credentials *Credentials, params map[string]string){
+const (
+  StatusMessageKind = 0
+  TweetMessageKind = 1
+)
+
+type Messager interface {
+  Kind() int
+  Tweet() *TweetMessage
+  Status() *StatusMessage
+}
+
+type StatusMessage struct {
+  StatusCode int
+  Error bool
+}
+
+func (m *StatusMessage) Kind() int {
+  return StatusMessageKind
+}
+
+func (m *StatusMessage) Status() *StatusMessage {
+  return m
+}
+
+func (m *StatusMessage) Tweet() *TweetMessage {
+  return &TweetMessage{}
+}
+
+
+
+type TweetMessage struct {
+  Body string
+}
+
+func (m *TweetMessage) Kind() int {
+  return TweetMessageKind
+}
+
+func (m *TweetMessage) Tweet() *TweetMessage {
+  return m
+}
+
+func (m *TweetMessage) Status() *StatusMessage {
+  return &StatusMessage{} 
+}
+
+func TwitterStream (ch chan Messager, credentials *Credentials, params map[string]string){
   c := oauth.NewCredentials(credentials.oauth_consumer_key, credentials.oauth_token, credentials.oauth_consumer_secret, credentials.oauth_token_secret)
 
   r, _ := oauth.NewRequest("POST", "https://stream.twitter.com/1.1/statuses/filter.json", params, c)
@@ -27,16 +68,35 @@ func TwitterStream (ch chan Tweet, credentials *Credentials, params map[string]s
   client := http.Client{}
   resp, _ := client.Do(r.HttpRequest())
 
-  fmt.Println(resp.Status)
+  message := &StatusMessage{
+    StatusCode: resp.StatusCode,
+    Error: false,
+  }
+
+  ch <- message
 
   body_reader := bufio.NewReader(resp.Body)
   for {
-    line, prefix, err := body_reader.ReadLine()
+    var part []byte //Part of line
+    var prefix bool //Flag. Readln readed only part of line.
+    var sep []byte // Separator for Join function
+
+    parts := make([][]byte, 0, 5)
+
+    part, prefix, err := body_reader.ReadLine()
+    parts = append(parts, part)
+
+    for !prefix {
+      part, prefix, err = body_reader.ReadLine()
+      parts = append(parts, part)
+    }
+
     if err != nil { break }
 
-    fmt.Print(string(line))
-    if !prefix {
-      fmt.Println("\n----------------------------")
+    tweet := &TweetMessage{
+      Body: string(bytes.Join(parts, sep)),
     }
+
+    ch <- tweet
   }
 }
--- twitter_api_test.go
+++ twitter_api_test.go
@@ -12,7 +12,7 @@ type OAuthSuite struct{}
 var _ = Suite(&OAuthSuite{})
 
 func (s *OAuthSuite) TestCreateClient(c *C) {
-  ch := make(chan Tweet)
+  ch := make(chan Messager)
   params := make(map[string]string, 1)
 
   credentials := Credentials{
@@ -22,12 +22,18 @@ func (s *OAuthSuite) TestCreateClient(c *C) {
     oauth_token_secret: "tO5hW1ye3myBnT78DspVbTKWFgadvKeU1EOiV3o5Tg",
   }
 
-  params["track"] = "golang"
+  params["track"] = "twitter"
 
   go TwitterStream(ch, &credentials, params)
 
   for {
     t := <- ch
-    fmt.Println("Recieve tweet: ", t.text)
+    fmt.Println("Recieve message with kind = ", t.Kind())
+    switch t.Kind() {
+    case StatusMessageKind:
+      fmt.Println("Status = ", t.Status().StatusCode)
+    case TweetMessageKind:
+      fmt.Println("Tweet body = ", t.Tweet().Body)
+    }
   }
 }
